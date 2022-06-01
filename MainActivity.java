c7c73c5f84cfaf9adfea06c528f67dd11a99596d
Enuar Dvd Lara Rjs <enuar2110@gmail.com>
2022-05-31 19:51:18 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,8 @@ package co.edu.cesde;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +17,7 @@ import co.edu.cesde.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mainBinding;
+    private DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +25,28 @@ public class MainActivity extends AppCompatActivity {
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = mainBinding.getRoot();
         setContentView(view);
-    }
-
-    public void showMessage(View view) {
-        Toast.makeText(this, "Hola, Ingresa tus datos para continuar", Toast.LENGTH_SHORT).show();
-        mainBinding.tvPassword.setText("Contraseña");
+        dbHelper = new DBHelper(this);
     }
 
     public void signUp(View view) {
         Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
         startActivity(intent);
+    }
+
+    public void signIn(View view) {
+        String email= mainBinding.etEmail.getText().toString();
+        String password= mainBinding.etPassword.getText().toString();
+        if (email.isEmpty() && password.isEmpty()){
+            Toast.makeText(this, "Debe llenar todos los campos", Toast.LENGTH_SHORT).show();
+        }else{
+            SQLiteDatabase db= dbHelper.getWritableDatabase();
+            String sql= String.format("SELECT * FROM users WHERE email='%s' AND password='%s'",email,password);
+            Cursor cursor = db.rawQuery(sql, null);
+            if (cursor.getCount()>0){
+                Toast.makeText(this, "Usuario Correcto", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "Usuario no Existe", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
